c2f05c31970c9e734ca0e067af13a941c7845afa
CK <chitrakoneti@gmail.com>
2024-11-01 21:11:30 +1100

    Update pricingRules.ts

--- src/pricingRules.ts
+++ src/pricingRules.ts
@@ -1,18 +1,15 @@
-export class PricingRules{
-
-    protected priceRule: Map<string, number>;; 
-    constructor(){
+export class PricingRules {
+    protected priceRule: Map<string, number>;;
+    constructor() {
         this.priceRule = new Map<string, number>
     }
-    //
-    setRules(item:string, price:number){
+    setRules(item: string, price: number) {
         this.priceRule.set(item, price)
     }
-
-    removeRules(item:string){
+    removeRules(item: string) {
         this.priceRule.delete(item)
     }
-    getRules(item:string){
+    getRules(item: string) {
         return this.priceRule.get(item)
     }
 }
